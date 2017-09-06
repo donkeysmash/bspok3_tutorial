669724c6ba9b20c78f5076320bc6fc42f986c07f
ken lee <clee@bespokemetrics.com>
2017-09-06 17:44:41 -0400

    redux tutorial

---
+++ fe/redux-tutorial/src/reducers.js
@@ -0,0 +1,43 @@
+import {
+  VisibilityFilters,
+  ADD_TODO, TOGGLE_TODO
+} from './actions';
+
+const initialState = {
+  todos: [],
+  visibilityFilter: VisibilityFilters.SHOW_ALL
+};
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      });
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      });
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            })
+          }
+        })
+      });
+    default:
+      return state;
+  }
+}
+
+
+
