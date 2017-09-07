642c87fe6d25c162a11dc7924dfaa8eed97835ef
ken lee <clee@bespokemetrics.com>
2017-09-07 17:45:05 -0400

    connection between app and mongo in docker

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import logging
 import pprint
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'animal'
+app.config['MONGO_HOST'] = 'mongo'
+
 api = Api(app)
 mongo = PyMongo(app)
 
 
 class HelloWorld(Resource):
     def get(self):
-        logging.info('getting')
-        print('getting...')
-        return {'hello': 'worlddddd'}
+        one_donkey = mongo.db.donkeys.find_one()
 
+        return {
+          'name': str(one_donkey)
+        }
 
 api.add_resource(HelloWorld, '/')
 
